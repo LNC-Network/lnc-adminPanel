774bdae4e4ebbf85aafdb0910159c7be95372fd1
Jit Debnath <jit.nathdeb@gmail.com>
2024-10-20 22:59:15 +0530

    all

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import Modal from "react-modal";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -53,31 +54,42 @@ export default function Login() {
       )}
 
       <div
-        className={`flex flex-col items-center justify-center h-screen ${
+        className={`h-screen flex items-center justify-center ${
           isDesktop ? "w-1/2" : "w-full"
         }`}
       >
-        <h1 className="text-3xl mb-6">Login</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="p-2 border"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="p-2 border"
-          />
-          <button type="submit" className="p-2 bg-blue-500 text-white">
-            Login
-          </button>
-        </form>
-        {error && <p className="text-red-500 mt-4">{error}</p>}
+        <div className="bg-slate-200 backdrop-blur-2xl flex flex-col items-center justify-evenly py-12 px-8">
+          <h1 className="text-3xl">Login</h1>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col space-y-4 px-4 py-8"
+            autoComplete="off"
+          >
+            <input
+              type="text"
+              placeholder="Name Code"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="p-2 outline-blue-500"
+            />
+            <input
+              type="password"
+              placeholder="Pass Code"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="p-2 outline-blue-500"
+            />
+            <button type="submit" className="p-2 bg-blue-500 text-white">
+              Login
+            </button>
+          </form>
+        </div>
+
+        {error &&
+          (() => {
+            alert("Wrong Credentials");
+            return null;
+          })()}
       </div>
     </div>
   );
